257b0e8b1f939f61aa55ccb0f4b4da8cc91eb62c
Jonathan Small <jonathn6@icloud.com>
2018-09-06 09:21:18 -0400

    Initial commit

---
+++ src/screens/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as Enterproperty} from './enterproperty';
+export { default as LoadProperty} from './loadProperty';
+export { default as Editproperty} from './editproperty';
